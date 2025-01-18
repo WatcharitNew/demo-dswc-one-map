e5e53ae22abf0da29265109a3646fdf397841f0d
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-01-19 01:43:51 +0700

    feat: use image from mantine instead

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+"use client";
+
+import { Image } from "@mantine/core";
 
 export const AppLayout = ({ children }) => {
   return (
@@ -7,11 +9,9 @@ export const AppLayout = ({ children }) => {
         <div className="flex-grow">
           <div className="flex flex-row items-center gap-2">
             <Image
-              src="/termtem-logo.svg"
               alt="termtem logo"
-              width={56}
-              height={56}
-              priority
+              className="size-14"
+              src="/termtem-logo.svg"
             />
             <p className="font-medium text-gray-600 text-lg">
               ระบบข้อมูลแผนที่สำหรับการบริหารจัดการภัยพิบัติ
@@ -21,9 +21,9 @@ export const AppLayout = ({ children }) => {
         </div>
 
         <div className="flex-none">
-          <div className="flex flex-row gap-4">
-            <Image src="/bell.svg" alt="bell" width={29} height={30} />
-            <Image src="/profile.svg" alt="profile" width={56} height={56} />
+          <div className="flex flex-row gap-4 items-center">
+            <Image alt="bell" className="size-8" src="/bell.svg" />
+            <Image alt="profile" className="size-14" src="/profile.svg" />
             <div className="flex flex-col text-black">
               <p>Admin</p>
               <p>ผู้ว่าราชการจังหวัด</p>
@@ -33,23 +33,17 @@ export const AppLayout = ({ children }) => {
       </div>
       <div className="w-full flex justify-center relative -mt-3">
         <Image
-          src="/navbar-curve.svg"
           alt="navbar-curve"
-          width={1440}
-          height={56}
-          priority
+          src="/navbar-curve.svg"
+          w={1440}
+          h={56}
           className="drop-shadow-[0_10px_10px_rgba(179,213,249,0.5)] z-10"
         />
         <p className="text-2xl text-blue-900 absolute left-1/2 top-4 -ml-[2.125rem] z-10">
           แผนที่จังหวัดเชียงราย
         </p>
         <div className="absolute right-0 top-6 h-16 w-2/5">
-          <Image
-            src="/dss-bg.svg"
-            alt="dss-bg"
-            layout="fill"
-            objectFit="contain"
-          />
+          <Image alt="dss-bg" className="w-full h-full" src="/dss-bg.svg" fit='contain' />
           <div className="h-10 w-[20.625rem] px-7 py-2 bg-white right-0 top-3 absolute border-l border-l-gray-200">
             <p className="text-right text-gray-900">
               เข้าสู่ระบบ การสนับสนุนการตัดสินใจ (DSS)
