f07975cebd68c462f95806709663aa633641a8c4
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-01-21 01:29:18 +0700

    feat: add transition to disaster label

--- src/components/MapLabel.jsx
+++ src/components/MapLabel.jsx
@@ -1,48 +1,60 @@
 "use client";
 
-import { useMemo } from "react";
-
 import { FLOOD_LEVEL, INFORMATION, MUDSLIDE } from "@/constants";
 
-import { ColorSwatch, Flex, Text } from "@mantine/core";
+import { ColorSwatch, Flex, Text, Transition } from "@mantine/core";
 
-export const MapLabel = ({selectedDisaster}) => {
-  const isSelectedFlood = useMemo(() => {
-    return selectedDisaster.includes('flood')
-  }, [selectedDisaster])
-
-  const isSelectedMudslide = useMemo(() => {
-    return selectedDisaster.includes('mudslide')
-  }, [selectedDisaster])
-
+export const MapLabel = ({ selectedDisaster }) => {
   return (
     <Flex className="flex-col items-end gap-3 py-2 px-6 bg-white rounded-md">
-      {isSelectedFlood && <Flex className="gap-5 items-center">
-        <Text>อุทกภัย:</Text>
+      <Transition
+        mounted={selectedDisaster.includes("flood")}
+        transition="fade-right"
+        duration={400}
+        timingFunction="ease"
+      >
+        {(style) => (
+          <div style={style}>
+            <Flex className="gap-5 items-center">
+              <Text>อุทกภัย:</Text>
 
-        {FLOOD_LEVEL.map((level) => (
-          <Flex key={level.title} className="items-center gap-2">
-            <ColorSwatch color={level.color} radius="sm" size={16} />
-            <Text>{level.title}</Text>
-          </Flex>
-        ))}
-      </Flex>}
+              {FLOOD_LEVEL.map((level) => (
+                <Flex key={level.title} className="items-center gap-2">
+                  <ColorSwatch color={level.color} radius="sm" size={16} />
+                  <Text>{level.title}</Text>
+                </Flex>
+              ))}
+            </Flex>
+          </div>
+        )}
+      </Transition>
 
-      {isSelectedMudslide && <Flex className="gap-5 items-center">
-        <Text>ดินโคลนถล่ม:</Text>
+      <Transition
+        mounted={selectedDisaster.includes("mudslide")}
+        transition="fade-right"
+        duration={400}
+        timingFunction="ease"
+      >
+        {(style) => (
+          <div style={style}>
+            <Flex className="gap-5 items-center">
+              <Text>ดินโคลนถล่ม:</Text>
 
-        {MUDSLIDE.map((level) => (
-          <Flex key={level.title} className="items-center gap-2">
-            <ColorSwatch color={level.color} radius="sm" size={16} />
-            <Text>{level.title}</Text>
-          </Flex>
-        ))}
-      </Flex>}
+              {MUDSLIDE.map((level) => (
+                <Flex key={level.title} className="items-center gap-2">
+                  <ColorSwatch color={level.color} radius="sm" size={16} />
+                  <Text>{level.title}</Text>
+                </Flex>
+              ))}
+            </Flex>
+          </div>
+        )}
+      </Transition>
 
       <Flex className="gap-5 items-center">
         {INFORMATION.map((information) => (
           <Flex key={information.key} className="items-center gap-2">
-            <information.icon className="size-6 text-blue-400"/>
+            <information.icon className="size-6 text-blue-400" />
             <Text>{information.title}</Text>
           </Flex>
         ))}
