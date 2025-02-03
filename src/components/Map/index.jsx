24552d5b6442911d09b4c6d65d3a4bf1f52d9522
WasitaL <wasita.pn@gmail.com>
2025-02-04 00:47:58 +0700

    feat: arcgis map

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { useEffect, useRef } from "react";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import Zoom from "@arcgis/core/widgets/Zoom";
+import Expand from "@arcgis/core/widgets/Fullscreen";
+import "./index.css";
+
+export const ArcgisMap = () => {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      const map = new Map({
+        basemap: "topo",
+      });
+
+      const view = new MapView({
+        container: mapRef.current,
+        map: map,
+        center: [100.11, 14.47],
+        zoom: 10,
+      });
+
+      const featureLayer1 = new FeatureLayer({
+        url: "https://gis-portal.disaster.go.th/arcgis/rest/services/Hosted/DevMap_SPR/FeatureServer/0",
+      });
+      const featureLayer2 = new FeatureLayer({
+        url: "https://gis-portal.disaster.go.th/arcgis/rest/services/Hosted/DevMap_SPR/FeatureServer/1",
+      });
+      const featureLayer3 = new FeatureLayer({
+        url: "https://gis-portal.disaster.go.th/arcgis/rest/services/Hosted/DevMap_SPR/FeatureServer/2",
+      });
+      const featureLayer4 = new FeatureLayer({
+        url: "https://gis-portal.disaster.go.th/arcgis/rest/services/Hosted/DevMap_SPR/FeatureServer/3",
+      });
+      const featureLayer5 = new FeatureLayer({
+        url: "https://gis-portal.disaster.go.th/arcgis/rest/services/Hosted/DevMap_SPR/FeatureServer/4",
+      });
+      const featureLayer6 = new FeatureLayer({
+        url: "https://gis-portal.disaster.go.th/arcgis/rest/services/Hosted/DevMap_SPR/FeatureServer/5",
+      });
+
+      map.add(featureLayer1);
+      map.add(featureLayer2);
+      map.add(featureLayer3);
+      map.add(featureLayer4);
+      map.add(featureLayer5);
+
+      const zoomWidget = new Zoom({
+        view: view,
+      });
+
+      const expandWidget = new Expand({
+        view: view,
+      });
+      view.ui.add(zoomWidget, "top-right");
+      view.ui.add(expandWidget, "top-right");
+    }
+  }, [mapRef]);
+
+  return <div className="w-full max-h-[42rem] h-[42rem]" ref={mapRef}></div>;
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-export * from './AppLayout'
-export * from './MapLabel'
+export * from "./AppLayout";
+export * from "./MapLabel";
+export * from "./Map";
